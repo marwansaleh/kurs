776db3f6049f80e478e51f6f4e550095c3c22cbf
marwansaleh <marwansaleh@192.168.1.102>
2015-12-07 00:20:10 +0700

    Initial commit

---
+++ config/production/database.php
@@ -0,0 +1,6 @@
+<?php
+$config ['db_dbhost'] = 'localhost';
+$config ['db_dbname'] = 'k2427808_main';
+$config ['db_dbuser'] = 'k2427808_indon1';
+$config ['db_dbpassword'] = '2270hmdwce';
+$config ['db_port'] = 3306;
